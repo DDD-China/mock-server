53048f05f4e6e202b31abcece62aad5d928733f8
linwenjun <linwenjun@163.com>
2018-11-27 19:44:48 +0800

    add submodule

--- src/main/java/capbility/thoughtworks/com/stubrunner/StubRunnerApplication.java
+++ src/main/java/capbility/thoughtworks/com/stubrunner/StubRunnerApplication.java
@@ -2,12 +2,10 @@ package capbility.thoughtworks.com.stubrunner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.contract.stubrunner.server.EnableStubRunnerServer;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
 @SpringBootApplication
-@EnableStubRunnerServer
+@AutoConfigureWireMock
 public class StubRunnerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StubRunnerApplication.class, args);
