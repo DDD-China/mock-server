fac62d5b40d5039f3e0a444074ca73c14f34b995
linwenjun <linwenjun@163.com>
2018-11-28 10:41:30 +0800

    Removed submodule

--- src/main/java/capbility/thoughtworks/com/stubrunner/StubRunnerApplication.java
+++ src/main/java/capbility/thoughtworks/com/stubrunner/StubRunnerApplication.java
@@ -2,10 +2,12 @@ package capbility.thoughtworks.com.stubrunner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
 @SpringBootApplication
 @AutoConfigureWireMock
+@AutoConfigureStubRunner
 public class StubRunnerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StubRunnerApplication.class, args);
